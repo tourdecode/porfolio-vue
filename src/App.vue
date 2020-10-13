46bd3fd2f42fd40718f55d0159a3cf37ed90a8b5
drafdecode <d055222148@gmail.com>
2020-10-13 21:51:24 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/wewebplus_05478.jpg"> -->
+  <home/>
+</template>
+
+<script>
+import Home from './views/Home'
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/profile.js
@@ -0,0 +1,168 @@
+export default {
+    fullname: "Peerapat Matheang",
+    nickname: "@drafdecode",
+    birthyear: 1996,
+    tel: "096-756-7844",
+    line: "progame69",
+    mail: "peerapatmatheang@gmail.com",
+    county: "Thailand",
+    address: "Bangkok",
+    freelance: "Available",
+    about: "I am a working style planner. And web developer, i graduated directly in software development And develop websites for many well-known national and government agencies and over the past 10 years I have experienced almost all types of websites.",
+    images: "/images/profile/wewebplus-05457-960x960.jpg",
+    no_images: "/images/profile/wewebplus-05457-960x960.jpg",
+    social: {
+        facebook: "https://facebook.com/drafmatong",
+        linkedin: "https://www.linkedin.com/in/พีรพัฒน์-มาเที่ยง-0ba861157/",
+        github: "https://github.com/drafdecode",
+    },
+    work:[
+        "Software Engineer",
+        "Web Developer",
+        "FullStack Developer",
+        "Research and Development",
+    ],
+    certificates: [
+        {
+            title: "Certificate ITPE<br>IP",
+            category: "สวทช.",
+            images: [
+                "/images/profile/itpe1.jpg",
+                "/images/profile/itpe2.jpg",
+            ],
+        },
+        {
+            title: "Certificate TPQI<br>นักพัฒนาระบบชั้น 3",
+            category: "สถาบันคุณวุฒิวิชาชีพ",
+            images: [
+                "/images/profile/tpqi1.jpg",
+                "/images/profile/tpqi2.jpg",
+            ],
+        },
+        {
+            title: "Certificate IC3",
+            category: "สถาบันคุณวุฒิวิชาชีพ",
+            images: [
+                "/images/profile/ic3.jpg",
+            ],
+        },
+    ],
+    awards: {
+        fullname: "Peerapat Matheang",
+        images: "/images/profile/wewebplus-05971-172x172.jpg",
+        work: "FullStack Developer",
+        desc: "All awards received since college.",
+        list: [
+            {
+                title: "04 September 2016",
+                desc: "ACM ICPC 2016 รองชนะเลิศอันดับ 1 ระดับภูมิภาคเหนือ",
+            },
+            {
+                title: "17 - 18 October 201",
+                desc: "ACM ICPC 2016 ระดับประเทศ Thailand National On-site",
+            },
+            {
+                title: "23 June 2017",
+                desc: "Startup Thailand 2017",
+            },
+            {
+                title: "01 April 2018",
+                desc: "Startup Thailand 2018",
+            },
+            {
+                title: "05 - 07 May 2017",
+                desc: "NDX-Northern Digital Expo 2017",
+            },
+            {
+                title: "06 February 2017",
+                desc: "การแข่งขันพัฒนาโปรแกรมคอมพิวเตอร์แห่งประเทศไทย ครั้งที่ 19 (NSC 2017)",
+            },
+            {
+                title: "14 February 2018",
+                desc: "การแข่งขันพัฒนาโปรแกรมคอมพิวเตอร์แห่งประเทศไทย ครั้งที่ 20 (NSC 2018)",
+            },
+            {
+                title: "30 November 2016",
+                desc: "UP Disrupt 2016",
+            },
+        ],
+    },
+    resume: {
+        experiences: [
+            {
+                isActive: true,
+                year: "2020 - Present",
+                subject: "FullStack Developer",
+                title: "289 Production House Co,. Ltd.",
+                desc: "It is a web development company with government clients and private companies.",
+            },
+            {
+                isActive: true,
+                year: "2019 - Present",
+                subject: "CEO",
+                title: "Decodethailand Team.",
+                desc: "Work in a freeland as a team.",
+            },
+            {
+                isActive: false,
+                year: "2019 - 2020",
+                subject: "Web Developer &<br> Research and Development",
+                title: "Wewebplus Co,. Ltd.",
+                desc: "It is a web development company with government clients and private companies.",
+            },
+        ],
+        educations: [
+            {
+                isActive: false,
+                year: "2015 - 2019",
+                subject: "University Of Phayao",
+                title: "Phayao, Thailand",
+                desc: "It is a public university and I am the Northern Quota Entrance Examination of the Department of Engineering in Software Engineering.",
+            },
+            {
+                isActive: false,
+                year: "2009 - 2015",
+                subject: "Phromphiramwittaya School",
+                title: "Phitsanulok, Thailand",
+                desc: "Being a high school, I studied science and mathematics room 6/1.",
+            },
+        ],
+    },
+    skills: [
+        {
+            title: "The Ability to Communicate",
+            scale: "70%",
+        },
+        {
+            title: "Always seeking new knowledge",
+            scale: "90%",
+        },
+        {
+            title: "Reasoning and Problem Solving",
+            scale: "85%",
+        },
+        {
+            title: "Enthusiasm for work",
+            scale: "75%",
+        },
+    ],
+    languages: [
+        {
+            title: "Thai",
+            scale: "90%", //10-100
+        },
+        {
+            title: "English",
+            scale: "30%",
+        },
+    ],
+    knowledges: [
+        "Adaptability",
+        "Strong Research Skills",
+        "The Ability to Get Focused",
+        "Communicate",
+        "The Ability to Meet Deadlines",
+        "Organizational Skills",
+        "Always seeking new knowledge",
+    ],
+}
